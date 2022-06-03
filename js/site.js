eac7e15eeee4fd30ff2867c57cd035798e2b9b56
Robert Stutzman <94028578+RobertStutzman@users.noreply.github.com>
2022-06-02 19:38:50 -0700

    finished psuedo coding site.js

---
+++ js/site.js
@@ -0,0 +1,48 @@
+//get the values from the user. We need to get the Fizz and Buzz value
+function getValues(){
+
+    //get the users values from the DOM
+    let fizzValue = document.getElementById("fizzValue").getValues
+
+    //parse for numbers
+
+    //check that they the numbers are integers
+
+    //we call fizzBuzz
+
+    //take return from fizzBuzz and call displayData
+}
+
+
+//Generate the values
+function fizzBuzz(fizzValue, buzzValue){
+
+    let returnArray = [];
+    //intialize the returnArray
+
+    //write a loop from 1-100
+
+    //we need to check the current value in three steps
+    //check to see if divisable by both (3 and 5 for example)
+        //if so, push "FizzBuzz" into an array not the number
+
+    //check to see if divisible by fizz value (3 for example)
+        //if so, push "Fizz" into an array and not the number
+
+    //check to se if divisible by buzz value (5 for example)
+        //if so, push "Buzz" into an array and not the number
+
+    //if neither, push the number into the array
+
+    returnArray;
+}
+
+//dispay the values
+function displayData(fbArray){
+
+    //loop over the array and create a tablerow for each item
+
+    //add all the rows to the table. 
+}
+
+
